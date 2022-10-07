a7e9b10ccb01591292d6f83e802f0be4c0f4f42d
sam_dev <samueldevpro09@gmail.com>
2022-10-07 16:08:34 +0200

    header added

---
+++ components/Header.js
@@ -0,0 +1,81 @@
+import { BiMenuAltRight } from "react-icons/bi";
+import { BiUser, BiHomeAlt } from "react-icons/bi";
+import { CgWorkAlt } from "react-icons/cg";
+import { HiOutlineDocumentDuplicate } from "react-icons/hi";
+
+import { useState } from "react";
+
+const Header = () => {
+  const [isMenuOpen, setIsmenuOpen] = useState(false);
+
+  return (
+    <div className="md:max-w-7xl xl:max-w-screen-xl mx-auto px-5 py-5 flex items-center justify-between md:px-5 xl:px-0 ">
+      <div>
+        <img src="/img/logo.png" alt="logo" className="logo" />
+      </div>
+      {/* desktop only */}
+      <div className="menu hidden md:block">
+        <ul>
+          <li>
+            <a className="active" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projets</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="contact hidden md:block">
+        <button>Contact</button>
+      </div>
+
+      {/* mobile only */}
+      <div className="icon-wr md:hidden" onClick={() => setIsmenuOpen(true)}>
+        <BiMenuAltRight className="icon" />
+      </div>
+
+      {isMenuOpen && (
+        <>
+          <div
+            className="sidemenu md:hidden"
+            onClick={() => setIsmenuOpen(false)}
+          ></div>
+
+          <div className="content shadow-xl">
+            <ul>
+              <li>
+                <BiHomeAlt />
+                <a className="active" href="#">
+                  Home
+                </a>
+              </li>
+              <li>
+                <BiUser />
+                <a href="#">About</a>
+              </li>
+              <li>
+                <CgWorkAlt />
+                <a href="#">Projets</a>
+              </li>
+              <li>
+                <HiOutlineDocumentDuplicate />
+                <a href="#">Blog</a>
+              </li>
+            </ul>
+            <button>Contact</button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-full">
       <Head>
         <title>Home</title>
         <meta name="description" content="SamuelAmz Home Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-2xl">lorem</h1>
+      <div>
+        <Header />
+        <Footer />
+      </div>
     </div>
   );
 }
